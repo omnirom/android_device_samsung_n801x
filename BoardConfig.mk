0ddf74722b282e71fe7036d4a9aa732cee91ae21
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-02-09 19:15:06 -0800

    n801x: Detect device variant and set properties accordingly.
    Change-Id: I89d1318a33e12a64256dbcede366a1a133c24d05

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,3 +27,5 @@ TARGET_OTA_ASSERT_DEVICE := c0,p4notewifi,p4notewifiww,n8013,GT-N8013,n8010,GT-N
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/n801x/bluetooth
+
+TARGET_INIT_VENDOR_LIB := libinit_n801x
